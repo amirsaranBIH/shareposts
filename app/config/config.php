c90f377a5b80da878150b6084b7e401367b432a9
amirsaranBIH <amir.saranbih@gmail.com>
2018-05-04 21:12:25 +0200

    Initial commit and core framework with register and login views done and form validation

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASSWORD', '');
+  define('DB_NAME', 'shareposts');
+
+  // App Root
+  define('APPROOT', dirname(__FILE__, 2));
+
+  // URL Root
+  define('URLROOT', 'http://localhost/shareposts');
+
+  // Site Name
+  define('SITENAME', 'SharePosts');
+
+  // App version
+  define('APPVERSION', '1.0.0');
---
+++ app/views/inc/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <title><?php echo SITENAME; ?></title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/main.css">
+  </head>
+  <body>
+    <div class="container">
+    <?php require APPROOT . '/views/inc/navbar.php'; ?>
